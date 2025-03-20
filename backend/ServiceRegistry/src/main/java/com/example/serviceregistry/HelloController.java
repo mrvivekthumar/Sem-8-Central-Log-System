18bcb1e544302067194daa325cd119145e5f01d8
hetbhagatji09 <hetbhagatji09@gmail.com>
2025-03-20 22:54:04 +0530

    testing api for deployment

---
+++ backend/ServiceRegistry/src/main/java/com/example/serviceregistry/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.serviceregistry;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello from Service Registry!";
+    }
+}
