30d7c56ded00d88174b4abda2e7c9eb6e2594ba9
hetbhagatji09 <hetbhagatji09@gmail.com>
2025-03-30 19:47:51 +0530

    Remove Chat Service

--- backend/ChatService/src/main/java/com/example/chatservice/chatroom/ChatRoomRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.chatservice.chatroom;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import java.util.Optional;
-
-public interface ChatRoomRepository extends MongoRepository<ChatRoom, String> {
-    Optional<ChatRoom> findBySenderIdAndRecipientId(String senderId, String recipientId);
-}
